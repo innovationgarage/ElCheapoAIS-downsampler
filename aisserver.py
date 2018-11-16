3c02f5f80887319041425d01aec17d44a3c50291
asadisaghar <asadisaghar@gmail.com>
2018-11-16 13:20:48 +0100

    now is added as timestamp to the tagblock

--- aisserver.py
+++ aisserver.py
@@ -6,34 +6,44 @@ from __future__ import print_function
 from twisted.protocols import basic
 import ais.stream
 import ais
+import datetime
 
+def parse(line):
+    try:
+        return next(ais.stream.decode([line]))
+    except:
+        pass
+
+def add_timestamp(message):
+    timestamp = datetime.datetime.utcnow().strftime("%s")
+    tagblock = "\c:{}".format(timestamp)
+    checksum = ais.nmea.Checksum(tagblock)
+    tagblock = "{}*{}".format(tagblock, checksum)
+    message = "{}\{}".format(tagblock, message)
+    return message
+    
 class MyAIS(basic.LineReceiver):
     def connectionMade(self):
         print("Got new client!")
         self.factory.clients.append(self)
-
+        self.messages = []
+        
     def connectionLost(self, reason):
         print("Lost a client!")
         self.factory.clients.remove(self)
 
     def lineReceived(self, line):
-        print("received", repr(line))
+        self.nmealine = line
+        self.nmealine = add_timestamp(self.nmealine)
+        self.messages.append(parse(self.nmealine))
         for c in self.factory.clients:
-            c.message(line)
-            c.parse(line)
-
-    def nmeaFileReceived(self, nmeamsg):
-        print("received", repr(nmeamsg))
-        for c in self.factory.clients:
-            c.parse(nmeamsg)
+            c.echoMessage(len(self.messages))
+            c.echoMessage(self.messages[-1])
             
-    def message(self, message):
-        self.transport.write(message + b'\n')
+    def echoMessage(self, message):
+        self.transport.write(str(message) + b'\n')
 
-    def parse(self, message):
-        self.transport.write(str(ais.decode(message,0)))
-        
-        
+    
 from twisted.internet import protocol
 from twisted.application import service, internet
 

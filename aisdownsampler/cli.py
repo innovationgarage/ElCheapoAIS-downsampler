980115752bab3616129c2bf38fda43cd200101dc
Egil <egil.moller@freecode.no>
2019-08-29 16:11:39 +0200

    Notifier support

--- aisdownsampler/cli.py
+++ aisdownsampler/cli.py
@@ -35,16 +35,18 @@ def downsample(ctx, station_id, max_message_per_sec, max_message_per_mmsi_per_se
 @click.option('--station-id', default="unknown")
 @click.option('--max-message-per-sec', type=float)
 @click.option('--max-message-per-mmsi-per-sec', type=float)
+@click.option('--notifier', default=None)
 @click.argument("source")
 @click.argument("destination")
 @click.pass_context
-def server(ctx, source, destination, station_id, max_message_per_sec, max_message_per_mmsi_per_sec):
+def server(ctx, source, destination, station_id, max_message_per_sec, max_message_per_mmsi_per_sec, notifier):
     """Example:
 
 aisdownsampler server '{"type": "listen", "listen":"tcp:4711"}' '{"type": "listen", "listen":"tcp:4712"}'
 
 """
     aisdownsampler.server.server(
+        notifier = notifier,
         source = json.loads(source),
         destination = json.loads(destination),
         station_id = station_id,
--- aisdownsampler/server.py
+++ aisdownsampler/server.py
@@ -11,6 +11,8 @@ import aisdownsampler.downsampler
 import twisted.application.app
 import twisted.internet.reactor
 import twisted.application.strports
+import twisted.internet.task
+import datetime
 
 class Source(basic.LineReceiver):
     delimiter = '\n'
@@ -25,6 +27,7 @@ class Source(basic.LineReceiver):
 
     def lineReceived(self, line):
         print("Received", repr(line))
+        self.factory.server.last_input = datetime.datetime.now()
         if self.factory.server.destination is None: return        
         self.factory.server.destination.handleLine(line)
 
@@ -40,10 +43,16 @@ class Destination(basic.LineReceiver):
     def connectionMade(self):
         print("Got new destination client!")
         self.factory.server.destination = self
+        if self.factory.server.notifier:
+            with open(self.factory.server.notifier, "w") as f:
+                f.write("geocloud=1\n")
 
     def connectionLost(self, reason):
         print("Lost a client!")
         self.factory.server.destination = None
+        if self.factory.server.notifier:
+            with open(self.factory.server.notifier, "w") as f:
+                f.write("geocloud=0\n")
 
     def input(self):
         while True:
@@ -59,13 +68,28 @@ class Destination(basic.LineReceiver):
         for line in self.output:
             if line is None:
                 return
+            print("Sending", line.fullmessage)
             self.sendLine(line.fullmessage)
 
 class Server(object):
-    def __init__(self):
+    def __init__(self, notifier = None):
         self.source = None
         self.destination = None
-    
+        self.notifier = notifier
+        self.last_input = None
+        self.input_status = False
+        
+        self.heartbeat_task = twisted.internet.task.LoopingCall(self.heartbeat)
+        self.heartbeat_task.start(1.0)
+
+    def hearbeat(self):
+        input_status = self.last_input is not None and datetime.datetime.now() - self.last_input < datetime.timedelta(seconds = 1)
+        if input_status != self.input_status:
+            self.input_status = input_status
+            if self.notifier:
+                with open(self.notifier, "w") as f:
+                    f.write("nmea=%s\n" % ([0,1][self.input_status],))
+        
 class SourceFactory(protocol.ServerFactory):
     protocol = Source
     def __init__(self, server):
@@ -80,8 +104,8 @@ class DestinationFactory(protocol.ServerFactory):
         res.factory = self
         return res
 
-def build_server(source, destination, session, station_id):
-    server = Server()
+def build_server(source, destination, session, station_id, notifier):
+    server = Server(notifier = notifier)
     factories = {"source": SourceFactory(server),
                  "destination": DestinationFactory(server, session, station_id)}
     config = {"source": source, "destination": destination}
